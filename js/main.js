f1fae352f66fba7fe8d2b97048eacd2b88ab1278
Alisher <mamatkulovalisher20@gmail.com>
2023-07-28 17:11:23 +0500

    Js 1-dars
    22-guruh o'quvchisi

---
+++ js/main.js
@@ -0,0 +1,56 @@
+/*console.log("Men console oynasida ko'rinaman!");*/
+/*alert("Saytimizga hush kelibsiz!")*/
+/*
+document.write("Men body oynasida ko'rinaman!")*/
+
+/*let x = 2;*/
+/*let y = 5;*/
+/*x = 10;*/
+
+/*let x = 2, y = 5, z = 8;*/
+/*
+document.write(x+y+z)*/
+
+/*let  salom ="Assalomu alaykum";*/
+/*let rang ="#f1f1f1";*/
+/*document.write(rang);*/
+
+
+/*let x = 5;           */                           /*number*/
+/*let  matn = "JavaScript";     */                  /*string*/
+/*
+document.write(typeof matn);*/
+
+let x = 5;
+/*x+=5*/ /* x=5+5 */
+/*x-=3*/ /* x=5-3 */
+/*x*=1 */    /* x5*1 */
+/*x/=1  */    /* x=5/1 */
+/*let  daraja = x**5; */    /* 5*5 */
+--x;
+
+
+
+document.write(daraja);
+
+
+/*if (Agar)*/
+/*else (u holda)*/
+/*
+else if(yoki bo'lmasa)*/
+
+/*if("45" =="45"){
+    console.log("Ular teng!")
+}*/
+/*else{
+    console.log("Ular teng emas!")
+}*/
+if (45 == 45){
+    console.log("Birinchisidagi qiymatlar !")
+}
+else if(45 == 46){
+    console.log("Ikkinchisidagi qiymatlar teng!")
+}
+else {
+    console.log("Ikkalasidagi qiymatlar teng emas!")
+}
